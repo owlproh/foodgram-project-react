abaa99eb800c3648545348f2fa1480e7ef5f994d
Святослав Прохоренко <prosvo@yandex.ru>
2023-03-19 23:28:38 +0300

    srsly?

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -63,14 +63,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
@@ -84,24 +77,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': [
-            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
-        ],
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': [
-            'django.contrib.auth.password_validation.MinimumLengthValidator'
-        ],
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': [
-            'django.contrib.auth.password_validation.CommonPasswordValidator'
-        ],
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': [
-            'django.contrib.auth.password_validation.NumericPasswordValidator'
-        ],
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
